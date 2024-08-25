7a7830c082df986b894480c93930eb024d24f8d0
Ragu <rahulchalumuri54@gmail.com>
2024-08-25 16:13:55 -0400

    status code

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,7 +7,15 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL, json = input_json, headers=header)
-
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     dict_response = json.loads(response.text)
     emotion_predictions = dict_response['emotionPredictions'][0]['emotion']
     scores = {
@@ -19,14 +27,16 @@ def emotion_detector(text_to_analyze):
     }
 
     dominant_emotion = max(scores, key=scores.get)
-    return {
-        'anger': scores['anger'],
-        'disgust': scores['disgust'],
-        'fear': scores['fear'],
-        'joy': scores['joy'],
-        'sadness': scores['sadness'],
-        'dominant_emotion': dominant_emotion
-    }
+    if response.status_code == 200:
+        return {
+            'anger': scores['anger'],
+            'disgust': scores['disgust'],
+            'fear': scores['fear'],
+            'joy': scores['joy'],
+            'sadness': scores['sadness'],
+            'dominant_emotion': dominant_emotion
+        }        
+
 
 # {'emotionPredictions': [
 #     {'emotion': 
